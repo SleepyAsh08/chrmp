7a79b8b2e8c1b164f9e9e00276b904d8955a172e
Jan Andrianne Prollo <Mercadojan123@gmail.com>
2023-10-03 14:08:53 +0800

    Jan Updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <title>Sign In - Your System Name</title>
+    <title>7th CHRMP Conference</title>
     <link rel="icon" type="image/x-icon" href="/images/favicon.png">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
 </head>
@@ -23,7 +23,7 @@
                     </div>
                 </div>
                 <div class="row" style="color:white">
-                    <h3>YOUR INFORMATION SYSTEM NAME</h3>
+                    <h3>7th CHRMP Conference</h3>
                 </div>
             </div>
         </div>
@@ -31,25 +31,26 @@
             <div class="row text-center" id="mobile-logo">
                 <div class="col-offset-5 mb-1">
                     <img class="img-fluid" src="images/logo.png" alt="">
-                    <h3>YOUR INFORMATION SYSTEM NAME</h3>
+                    <h3>7th CHRMP Conference</h3>
                 </div>
             </div>
             <h4 class="fw-300 c-grey-900 mB-10">Login</h4>
             @if (count($errors) > 0)
-            <div class="alert alert-danger" style="margin-bottom: unset; padding: .3rem;">
-                @foreach($errors->all() as $message)
-                <small class="text-danger"><strong>
-                        <p style="margin-bottom: unset;">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
-                                class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16"
-                                role="img" aria-label="Warning:">
-                                <path
-                                    d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                            </svg>
-                            {{ $message }}</p>
-                    </strong></small>
-                @endforeach
-            </div>
+                <div class="alert alert-danger" style="margin-bottom: unset; padding: .3rem;">
+                    @foreach ($errors->all() as $message)
+                        <small class="text-danger"><strong>
+                                <p style="margin-bottom: unset;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"
+                                        fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2"
+                                        viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                                        <path
+                                            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                                    </svg>
+                                    {{ $message }}
+                                </p>
+                            </strong></small>
+                    @endforeach
+                </div>
             @endif
             <form method="POST" action="{{ route('login') }}">
                 @csrf
@@ -69,7 +70,8 @@
                                     class="peers peer-greed js-sb ai-c form-label"><span
                                         class="peer peer-greed">Remember Me</span></label></div>
                         </div>
-                        <div class="peer"><button type="submit" class="btn btn-primary btn-color">Login</button></div>
+                        <div class="peer"><button type="submit" class="btn btn-primary btn-color">Login</button>
+                        </div>
                     </div>
                 </div>
             </form>
@@ -78,19 +80,20 @@
 </body>
 
 <script>
-    window.addEventListener("load", (function () {
+    window.addEventListener("load", (function() {
         const t = document.getElementById("loader");
-        setTimeout((function () {
+        setTimeout((function() {
             t.classList.add("fadeOut")
         }), 300)
     }))
 
     var x = document.getElementById("mobile-logo");
 
-    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
 </script>
+
 </html>
